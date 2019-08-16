3ef899253c3d74ca97ad5a523ec5574df1c5287f
dnnyp <dnnypham@gmail.com>
2019-08-16 13:03:35 -0400

    Add feature to prevent users from selecting text
    Added a no-select class to all text fields on screen to prevent users
    from making any selections to the text. The x and o characters on the
    game board would previously highlight blue upon a double click.

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -4,7 +4,7 @@ const engine = require('./engine')
 const store = require('./../store')
 
 const newGameSuccess = data => {
-  $('#game-status').html('<h3>You started a new game!</h3>')
+  $('#game-status').html('<h3 class="no-select">You started a new game!</h3>')
   store.game = data.game
   store.currentPlayer = 'x'
   store.isOver = false
@@ -13,23 +13,23 @@ const newGameSuccess = data => {
 }
 
 const newGameFailure = () => {
-  $('#game-status').html('<h3>New game failure</h3>')
+  $('#game-status').html('<h3 class="no-select">New game failure</h3>')
 }
 
 const squareClickSuccess = data => {
   store.game = data.game
-  $('#square-' + store.index).text(store.currentPlayer)
+  $('#square-' + store.index).html('<span class="no-select">' + store.currentPlayer + '</span>')
   engine.changePlayer()
   const status = store.gameStatus === 'turn' ? store.currentPlayer.toUpperCase() + "'s turn" : store.gameStatus
-  $('#game-status').html('<h3>' + status + '</h3>')
+  $('#game-status').html('<h3 class="no-select">' + status + '</h3>')
 }
 
 const takenSquareFailure = () => {
-  $('#game-status').html('<h3>Square already taken!<h3>')
+  $('#game-status').html('<h3 class="no-select">Square already taken!</h3>')
 }
 
 const gameOverFailure = () => {
-  $('#game-status').html('<h3>Please start a new game!</h3>')
+  $('#game-status').html('<h3 class="no-select">Please start a new game!</h3>')
 }
 
 const getGamesPlayedSuccess = data => {
@@ -38,14 +38,14 @@ const getGamesPlayedSuccess = data => {
 
 const getGamesWonSuccess = data => {
   store.gamesWon = data.games.length
-  $('#message-container').append('<div class="alert alert-info ml-2" role="alert">Games Played: ' + store.gamesPlayed + ' | Games Won: ' + store.gamesWon + '</div>')
+  $('#message-container').append('<div class="alert alert-info no-select ml-2" role="alert">Games Played: ' + store.gamesPlayed + ' | Games Won: ' + store.gamesWon + '</div>')
   $('.alert').delay(4000).slideUp(200, function () {
     $(this).alert('close')
   })
 }
 
 const getStatsFailure = () => {
-  $('#message-container').append('<div class="alert alert-danger ml-2" role="alert">Failed getting stats</div>')
+  $('#message-container').append('<div class="alert alert-danger no-select ml-2" role="alert">Failed getting stats</div>')
   $('.alert').delay(1500).slideUp(200, function () {
     $(this).alert('close')
   })
